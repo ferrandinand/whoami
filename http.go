d0da867f7cf756113f13e14efe550ab398d388e0
Ferran Tomas <ferrandinand@users.noreply.github.com>
2018-10-24 13:54:00 +0200

    Added star

--- http.go
+++ http.go
@@ -16,7 +16,7 @@ func main() {
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
+        fmt.Fprintf(os.Stdout, "I'm a star%s\n", hostname)
  	fmt.Fprintf(w, "I'm a star %s\n", hostname)
     })
 
