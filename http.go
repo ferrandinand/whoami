ef91817908b3cfc3c9054cd0909031929979b35b
Ferran Tomas <ferrandinand@users.noreply.github.com>
2018-11-05 12:01:32 +0100

    Add new logging

--- http.go
+++ http.go
@@ -5,6 +5,8 @@ import (
   "fmt"
   "net/http"
   "log"
+
+  "github.com/golang/glog"
 )
 
 func main() {
@@ -14,10 +16,14 @@ func main() {
     }
 
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
+    log.SetOutput(os.Stderr)
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm a star %s\n", hostname)
+ 	      fmt.Fprintf(w, "I'm a star %s\n", hostname)
+        log.Println("Hello World")
+        log.Println("I am not a star")
+        log.Println("Myapp is running %s", hostname)
     })
 
 
